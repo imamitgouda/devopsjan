b90f492c0693504143c4e40a236271adc401d5e3
Amitkumar Gouda <Jan1user232@rpsconsulting.in>
2026-01-28 18:13:22 +0530

    Remove old files

--- src/main/java/com/wip/MainClass.java
+++
@@ -1,9 +0,0 @@
-package com.wip;
-
-public class MainClass {
-
-	public static void main(String[] args) {
-		System.out.println("Java For Docker!!!! . THis code is pulled from WIP GitHub Repository");
-	}
-
-}
